54673f78d57b1fc0b171f5e6d693d5122e77fdff
Leandro Afonso <leandro.afonso@hotmail.com>
2022-06-03 22:22:12 -0400

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import react from "react";
+
+function Keypad() {
+    function showChange() {
+        console.log("Entering password...")
+    }
+    return (
+        <input type="password" onChange={showChange}></input>
+    )
+}
+
+export default Keypad;
